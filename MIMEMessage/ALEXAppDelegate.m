3f51d186627770f31ce07c0561d6de056221a539
Alexander Kempgen <alex@kempgen.de>
2012-02-02 15:48:23 +0100

    Initial Commit

---
+++ MIMEMessage/ALEXAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ALEXAppDelegate.m
+//  MIMEMessage
+//
+//  Created by Alexander Kempgen on 02.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ALEXAppDelegate.h"
+
+@implementation ALEXAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
